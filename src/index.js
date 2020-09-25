4eca595caeab339c3081b35be13d8ed2a2d9aea8
EileenJuergens <juergens.eileen@gmail.com>
2020-09-25 15:57:51 +0200

    feat: finish part 6.1 - unicafe revisited

--- src/index.js
+++ src/index.js
@@ -6,21 +6,39 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+  const handleIncreaseGood = () => {
     store.dispatch({
       type: 'GOOD'
     })
   }
 
+  const handleIncreaseNeutral = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const handleIncreaseBad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+
+  const handleReset = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={handleIncreaseGood}>good</button> 
+      <button onClick={handleIncreaseNeutral}>neutral</button> 
+      <button onClick={handleIncreaseBad}>bad</button>
+      <button onClick={handleReset}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -9,27 +9,52 @@ describe('unicafe reducer', () => {
   }
 
   test('should return a proper initial state when called with undefined state', () => {
-    const state = {}
-    const action = {
-      type: 'DO_NOTHING'
-    }
-
-    const newState = counterReducer(undefined, action)
+    const newState = counterReducer(undefined, { type: 'DO_NOTHING' })
     expect(newState).toEqual(initialState)
   })
 
   test('good is incremented', () => {
-    const action = {
-      type: 'GOOD'
-    }
     const state = initialState
-
     deepFreeze(state)
-    const newState = counterReducer(state, action)
-    expect(newState).toEqual({
-      good: 1,
-      ok: 0,
-      bad: 0
-    })
+
+    const newState = counterReducer(state, { type: 'GOOD' })
+    expect(newState).toEqual({ good: 1, ok: 0, bad: 0 })
+  })
+
+  test('neutral is incremented', () => {
+    const state = initialState
+    deepFreeze(state)
+
+    const newState = counterReducer(state, { type: 'OK' })
+    expect(newState).toEqual({ good: 0, ok: 1, bad: 0 })
+  })
+
+  test('bad is incremented', () => {
+    const state = initialState
+    deepFreeze(state)
+
+    const newState = counterReducer(state, { type: 'BAD' })
+    expect(newState).toEqual({ good: 0, ok: 0, bad: 1 })
+  })
+
+  test('reset with zero', () => {
+    const state = initialState
+    deepFreeze(state)
+
+    const newState1 = counterReducer(state, { type: 'GOOD' })
+    expect(newState1).toEqual({ good: 1, ok: 0, bad: 0 })
+
+    const newState2 = counterReducer(newState1, { type: 'OK' })
+    expect(newState2).toEqual({ good: 1, ok: 1, bad: 0 })
+    
+    const newState3 = counterReducer(newState2, { type: 'OK' })
+    expect(newState3).toEqual({ good: 1, ok: 2, bad: 0 })
+    
+    const newState4 = counterReducer(newState3, { type: 'BAD' })
+    expect(newState4).toEqual({ good: 1, ok: 2, bad: 1 })
+
+    const newState5 = counterReducer(newState4, { type: 'ZERO' })
+    expect(newState5).toEqual({ good: 0, ok: 0, bad: 0 })
+    
   })
 })
