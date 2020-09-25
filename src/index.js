c5361ce4ae03d0f96fa7530a0e8eed004821226a
EileenJuergens <juergens.eileen@gmail.com>
2020-09-25 16:20:23 +0200

    feat: finish part 6.2 - unicafe revisited

--- src/index.js
+++ src/index.js
@@ -5,40 +5,66 @@ import reducer from './reducer'
 
 const store = createStore(reducer)
 
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>{text}</button>
+)
+
+const Statistic = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  if (!good && !neutral && !bad){
+    return <p>No feedback given yet.</p>
+  }
+  return (
+    <table>
+      <tbody>
+        <Statistic text="good" value={good}/>
+        <Statistic text="neutral" value={neutral}/>
+        <Statistic text="bad" value={bad}/>
+        <Statistic text="all" value={good+neutral+bad}/>
+        <Statistic text="average" value={(good*1+neutral*0+bad*-1)/(good+neutral+bad)}/>
+        <Statistic text="positive" value={`${(100*good)/(good+neutral+bad)} %`}/>
+      </tbody>
+    </table>
+  )
+}
+
 const App = () => {
   const handleIncreaseGood = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
+    store.dispatch({ type: 'GOOD' })
   }
 
   const handleIncreaseNeutral = () => {
-    store.dispatch({
-      type: 'OK'
-    })
+    store.dispatch({ type: 'OK' })
   }
 
   const handleIncreaseBad = () => {
-    store.dispatch({
-      type: 'BAD'
-    })
+    store.dispatch({ type: 'BAD' })
   }
 
   const handleReset = () => {
-    store.dispatch({
-      type: 'ZERO'
-    })
+    store.dispatch({ type: 'ZERO' })
   }
 
   return (
     <div>
-      <button onClick={handleIncreaseGood}>good</button> 
-      <button onClick={handleIncreaseNeutral}>neutral</button> 
-      <button onClick={handleIncreaseBad}>bad</button>
-      <button onClick={handleReset}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
+      <h2>Give Feedback</h2>
+      <Button onClick={handleIncreaseGood} text='good' /> 
+      <Button onClick={handleIncreaseNeutral} text='neutral' /> 
+      <Button onClick={handleIncreaseBad} text='bad' />
+      <Button onClick={handleReset} text='reset stats' />
+      <h2>The Statistics</h2>
+      <Statistics
+        good={store.getState().good}
+        neutral={store.getState().ok}
+        bad={store.getState().bad} />
     </div>
   )
 }
